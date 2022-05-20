407826fa1e288ce2c33e79250cd3653f87c671bb
howardyangyixuan <howardyangyixuan@gmail.com>
2022-05-20 19:52:55 +0800

    初始化home和statstics页面，并用grid布局

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react"
 import {Route, Routes} from "react-router-dom"
 
 import {Home} from "./pages/home"
+import {Statistics} from "./pages/statistics"
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Home/>}>
-        </Route>
+        <Route exact path="/" element={<Home/>}> </Route>
+        <Route exact path="/analysis" element={<Statistics/>}> </Route>
       </Routes>
     </div>
   )
--- src/pages/statistics.jsx
+++ src/pages/statistics.jsx
@@ -1,17 +1,16 @@
 import React from "react"
-import "./home.scss"
+import "./statistics.scss"
 
-export const Home = () => {
+export const Statistics = () => {
   return (
-    <div className="home">
-      <header>Silvia & Howard's Travel Plan</header>
+    <div className="statistics">
+      <header>Silvia & Howard's Travel Plan Statistics</header>
       <main>
         <section className="section1">1</section>
         <section className="section2">2</section>
         <section className="section3">3</section>
         <section className="section4">4</section>
         <section className="section5">5</section>
-        <section className="section6">6</section>
       </main>
     </div>
   )
