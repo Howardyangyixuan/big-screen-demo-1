856b7ea35cfd65e52986a3cbc74b2b467027f0f0
howardyangyixuan <howardyangyixuan@gmail.com>
2022-05-20 15:36:47 +0800

    初始化并设置rem

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import {Route, Routes} from "react-router-dom"
+
+import "./App.css"
+import {Home} from "./pages/home"
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Home/>}>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./home.scss"
+
+export const Home = () => {
+  return (
+    <div>你好</div>
+  )
+}
