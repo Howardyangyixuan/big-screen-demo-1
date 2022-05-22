a6dabc748e70d8c211514a814db24227210d4bcc
howardyangyixuan <howardyangyixuan@gmail.com>
2022-05-22 22:23:54 +0800

    优化chart7，添加chart8：饼图，用户分析 1. 通过内嵌一个饼图，实现内部显示标题，通过设置悬浮字体的宽高和背景色，仍保持原先效果 2. 通过formatter和itemStyle设置颜色，完全隐藏用于显示标题的饼图的legend

--- src/components/chart7.jsx
+++ src/components/chart7.jsx
@@ -11,16 +11,16 @@ export const Chart7 = () => {
       // tooltip: {
       //   trigger: 'item'
       // },
-      title: {
-        text: "年龄分布",
-        textStyle: {
-          fontSize: px(20)
-        },
-        left: "center",
-        top:"5%"
-      },
+      // title: {
+      //   text: "年龄分布",
+      //   textStyle: {
+      //     fontSize: px(20)
+      //   },
+      //   left: "center",
+      //   top: "5%"
+      // },
       legend: {
-        top: "75%",
+        top: "bottom",
         left: "center",
         itemWidth: px(20),
         itemHeight: px(20),
@@ -30,9 +30,9 @@ export const Chart7 = () => {
           name: "用户年龄分布",
           type: "pie",
           radius: ["50%", "70%"],
-          left: "0%",
-          top:"10%",
-          bottom: "20%",
+          top:-px(50),
+          // left: "0%",
+          // bottom: "20%",
           avoidLabelOverlap: false,
           label: {
             show: false,
@@ -44,6 +44,11 @@ export const Chart7 = () => {
             label: {
               show: true,
               formatter: "{b}\n共{c}人\n约{d}%",
+              backgroundColor: "pink",
+              fontSize:px(30),
+              width: px(160),
+              height: px(160),
+              borderRadius: px(80)
             }
           },
           labelLine: {
@@ -56,7 +61,24 @@ export const Chart7 = () => {
             {value: 384, name: "40-59岁"},
             {value: 210, name: "60岁以上"}
           ]
-        }
+        }, {
+          name: "用户年龄分布",
+          type: "pie",
+          radius: ["0%", "30%"],
+          top:-px(50),
+          itemStyle: {
+            color: "rgba(245, 194, 203, 0)"
+          },
+          label: {
+            show: true,
+            fontSize: px(30),
+            textBorderColor: "rgba(255, 0, 0, 0)",
+            fontWeight: "bold",
+            position: "center",
+            formatter:"年龄分布"
+          },
+          data: [{value: 0, name: ""}]
+        },
       ]
     })
     myChart.setOption(option)
