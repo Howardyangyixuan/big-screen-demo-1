bd96f2d8beca900d7a0ac2d2324aef40bbe718ec
howardyangyixuan <howardyangyixuan@gmail.com>
2022-05-22 18:10:56 +0800

    完成chart6地图： 1. 将svg更换为geoJSON可以通过center信息和name标识地图，并配合visualMap着色 2. 通过dataset替换series实现数据重用，避免重复写入数据 3. 通过Chart的on方法，监听mouseover/mouseout事件，通过seriesIndex建立两个series表之间的高亮链接 4. 自定义tooltip优化悬浮时的label展示 5. 添加toolbox，增加保存图片/浏览数据等功能 6. 通过map的属性scaleLimit/roam控制地图缩放，center/zoom控制动图展示，emphasis/showLegendSymbol调整高亮和显示效果

--- src/components/chart6.jsx
+++ src/components/chart6.jsx
@@ -1,74 +1,191 @@
 import {useEffect, useRef} from "react"
 import * as echarts from "echarts"
 import {px} from "../shared/fn"
-import svgChina from "../geo/中华人民共和国.svg"
+import geoJSON from "../geo/100000_full.json"
+import {createEchartsOptions} from "../shared/createEchartsOptions"
 
 export const Chart6 = () => {
   const ref = useRef(null)
   useEffect(() => {
-    var myChart = echarts.init(ref.current, null, {
-      renderer: "canvas",
-      useDirtyRect: false
-    })
-    var app = {}
-    var ROOT_PATH = "https://fastly.jsdelivr.net/gh/apache/echarts-website@asf-site/examples"
-    var option
-
-    const xhr = new XMLHttpRequest()
-    xhr.open("get", svgChina)
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
-          console.log(xhr.response)
-          echarts.registerMap("iceland_svg", {svg: xhr.response})
-
-          option = {
-            grid: {
-              left: '60%',
-              top: '20%',
-              bottom: '20%'
-            },
-            tooltip: {},
-            geo: {
-              tooltip: {
-                show: true
-              },
-              map: "iceland_svg",
-              roam: true,
-              zoom:3
-            },
-            series: {
-              type: "effectScatter",
-              coordinateSystem: "geo",
-              geoIndex: 0,
-              symbolSize: function (params) {
-                return (params[2] / 100)  + 5
-              },
-              itemStyle: {
-                color: "#b02a02"
-              },
-              encode: {
-                tooltip: 2
-              },
-              data: [
-                [116.40, 39.90, 100],
-                [121.47, 31.23, 110],
-                [120.15, 30.28, 80],
-                [116.98, 36.67, 61],
-                [113.27, 23.13, 70],
-                [104.07, 30.67, 81]
-              ].map((arr)=>[arr[0]+710,arr[1]+295,arr[2]])
+    // const myChart = echarts.init(ref.current, null, {
+    //   renderer: "canvas",
+    //   useDirtyRect: false
+    // })
+    const myChart = echarts.init(ref.current)
+    echarts.registerMap("China", geoJSON)
+    const option = createEchartsOptions({
+      tooltip: {
+        // formatter: "{b}:{c}人"
+      },
+      toolbox: {
+        show: true,
+        orient: 'vertical',
+        left: 'right',
+        top: 'center',
+        feature: {
+          dataView: { readOnly: false },
+          restore: {},
+          saveAsImage: {}
+        }
+      },
+      textColor: {
+        axisLine: {
+          lineStyle: {color: "#FFF"}
+        },
+      },
+      grid: {
+        left: "10%",
+        top: "50%",
+        bottom: "5%",
+        right: "5%"
+      },
+      dataset: {
+        // 用 dimensions 指定了维度的顺序。直角坐标系中，
+        // 默认把第一个维度映射到 X 轴上，第二个维度映射到 Y 轴上。
+        // 如果不指定 dimensions，也可以通过指定 series.encode
+        // 完成映射，参见后文。
+        dimensions: ["name", "value"],
+        source: [
+          {name: "北京市", value: 20057},
+          {name: "天津市", value: 1477},
+          {name: "重庆市", value: 1477},
+          {name: "上海市", value: 21686},
+          {name: "黑龙江省", value: 4918},
+          {name: "吉林省", value: 4368},
+          {name: "辽宁省", value: 3368},
+          {name: "河北省", value: 4737},
+          {name: "河南省", value: 4737},
+          {name: "湖北省", value: 15204},
+          {name: "湖南省", value: 10765},
+          {name: "山东省", value: 6992},
+          {name: "山西省", value: 3992},
+          {name: "陕西省", value: 5477},
+          {name: "广东省", value: 14045},
+          {name: "广西省", value: 3368},
+          {name: "江苏省", value: 8172},
+          {name: "江西省", value: 5243},
+          {name: "安徽省", value: 5932},
+          {name: "福建省", value: 3595},
+          {name: "浙江省", value: 7891},
+          {name: "香港特别行政区", value: 2318},
+          {name: "澳门特别行政区", value: 1218},
+          {name: "台湾省", value: 818},
+          {name: "海南省", value: 4518},
+          {name: "云南省", value: 3068},
+          {name: "贵州省", value: 4028},
+          {name: "四川省", value: 12178},
+          {name: "青海省", value: 2190},
+          {name: "甘肃省", value: 3338},
+          {name: "内蒙古自治区", value: 806},
+          {name: "新疆维吾尔自治区", value: 725},
+          {name: "西藏自治区", value: 227},
+          {name: "宁夏回族自治区", value: 463},
+          {name: "广西壮族自治区", value: 832},
+          {name: "境外IP", value: 5881},
+        ].sort((a, b) => b.value - a.value)
+      },
+      xAxis: {
+        type: "category",
+        axisLine: {
+          // lineStyle: {color: "#FFF"}
+        },
+        axisLabel: {
+          formatter(val) {
+            return val.substring(0, 2)
+          }
+        }
+      },
+      yAxis: {
+        axisLine: {
+          // lineStyle: {color: "#FFF"}
+        },
+      },
+      series: [
+        {
+          name:"用户人数",
+          type: "bar",
+          emphasis: {
+            focus: "self",
+          },
+          tooltip: {
+            formatter(val){
+              console.log(val)
+              return `${val.value.name} <br/> ${val.value.value}人`
             }
           }
-          myChart.setOption(option)
+        },
+        {
+          name: "用户人数",
+          type: "map",
+          roam: true,
+          select: {disabled: true},
+          center: [105, 23],
+          zoom: 1.2,
+          scaleLimit: {
+            max: 4,
+            min: 1
+          },
+          emphasis: {
+            itemStyle: {
+              areaColor: "#ffff66",
+            }
+          },
+          itemStyle: {
+            borderColor: "black",
+            borderWidth: px(2),
+          },
+          map: "China",
+          label: {
+            show: true
+          },
+          showLegendSymbol: true,
         }
-      }
-    }
-    xhr.send()
-
+      ],
+      visualMap: {
+        left: "80%",
+        bottom: "10%",
+        min: 1000,
+        max: 10000,
+        text: ["High", "Low"],
+        realtime: false,
+        calculable: true,
+        inRange: {
+          color: ["#5871c0", "#f3c2cb", "#fb607f"]
+        }
+      },
+    })
+    myChart.setOption(option)
+    myChart.on("mouseover", {seriesIndex: 0}, function (event) {
+      myChart.dispatchAction({
+        type: "highlight",
+        seriesIndex: 1,
+        name: event.name
+      })
+    })
+    myChart.on("mouseout", {seriesIndex: 0}, function (event) {
+      myChart.dispatchAction({
+        type: "downplay",
+        seriesIndex: 1,
+        name: event.name
+      })
+    })
+    myChart.on("mouseover", {seriesIndex: 1}, function (event) {
+      myChart.dispatchAction({
+        type: "highlight",
+        seriesIndex: 0,
+        name: event.name
+      })
+    })
+    myChart.on("mouseout", {seriesIndex: 1}, function (event) {
+      myChart.dispatchAction({
+        type: "downplay",
+        seriesIndex: 0,
+        name: event.name
+      })
+    })
   }, [])
   return (
-    <div className="map chart-wrapper">
+    <div className="map chart-wrapper border">
       <h2 className="title-border">用户IP归属地</h2>
       <div ref={ref} className="chart"/>
     </div>
